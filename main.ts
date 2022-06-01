4b153c1f5d74315ccc5e51b66131c604e5ab57db
pacobaor <98743296+pacobaor@users.noreply.github.com>
2022-06-01 08:45:55 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onPinPressed(TouchPin.P2, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
